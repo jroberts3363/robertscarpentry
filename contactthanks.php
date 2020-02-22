e03d87658beaae1de192326db99b57447c9f2c45
jroberts3363 <49343080+jroberts3363@users.noreply.github.com>
2020-02-22 17:03:16 -0500

    Add files via upload

---
+++ contactthanks.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<head>
+	<title>Simple Contact Form</title>
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+<body>
+
+
+		<h1>Your message has been sent!</h1><br />
+		<p>Thank you for your email. I'll get back to you shortly.</p> <br />
+		<p>Best regards,</p> <br />
+		<p>Ian Roberts</p>
+
+
+</body>
+</html>
